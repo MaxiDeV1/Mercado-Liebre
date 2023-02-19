bd9124d80999fe8f66b5e1747e80526c40426c45
MaxiDeV1 <maximovelasquez411@gmail.com>
2023-02-19 06:17:14 -0300

    Subida de archivos

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3001;
+app.use(express.static('public'));
+app.listen(port,() => console.log('Servidor en el puerto 3001'));
+
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'./views/register.html')));
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'./views/login.html')));
